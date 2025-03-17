71188ae3d778f70f5aec9cf34479cbd0fb9fce0e
Barat shankar <barathms96@gmail.com>
2025-03-17 10:16:23 +0530

    done

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div className="App">
      <h1>Counter App</h1>
-     <p>count: {count}</p>
-     <button onClick={increment} >Increment</button>
+     <p>Count: {count}</p>
+     <button onClick={increment}>Increment</button>
      <button onClick={decrement}>Decrement</button>
     </div>
   );
